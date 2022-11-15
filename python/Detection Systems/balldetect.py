bf03a0514a2cada0f1bdb76e71cbb5cfab661b7e
StormStarbrickYT <stormstarbrick@gmail.com>
2022-11-15 11:49:29 -0700

    Created balldetect.py

---
+++ python/Detection Systems/balldetect.py
@@ -0,0 +1,28 @@
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="max buffer size")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+pts = deque(maxlen=args["buffer"])
+# if a video path was not supplied, grab rthe reference
+# to the webcam
+if not args.get("video", false):
+    vs = VideoStream(src=0).start()
+# otherwise, grab a reference to the video file
+else:
+    vs = cv2.VideoCapture(args["video"])
+# allow the camera or video file to warm up
+time.sleep(2.0)
