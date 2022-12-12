9057e7e41d747b195fc859269eae0b2cf661dd80
StormStarbrickYT <stormstarbrick@gmail.com>
2022-12-12 08:06:23 +0000

    added some files

---
+++ code courses/C#/reeves.cs
@@ -0,0 +1,12 @@
+//This Code is Michael Reeves' This is copied from his videos on C#. All Rights go to Mykul. 
+
+namespace Meme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
