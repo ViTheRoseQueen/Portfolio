cec462809d0db926bc13fe65665eba605e395e14
Kristen Laurencelle <sakurastorm13@gmail.com>
2025-04-13 00:22:53 -0400

    Uploaded first file for python lessons. Credits are in the files.

---
+++ python/Learning files/Basics.py
@@ -0,0 +1,16 @@
+# Code written by Kristen Laurencelle
+# Tutorial from https://www.learnpython.org/
+#=========================================================
+
+# Prints a line to the terminal (Also known as a String)
+print("Hello World!")
+# Parentheses and quotations are required for a string to work
+
+# Python uses indentations in its code. This differs from other languages
+# where the indentations dont actually affect the code at all. 
+
+x = 1
+if x == 1:
+    # indented with 4 spaces or a tab. VSCode will automatically apply
+    # the indent for you.
+    print("x is 1.")
